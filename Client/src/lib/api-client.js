646ebdca311dedc00df0c0958cf9031756e0b770
Kshitiz Gupta <106856399+kshitizGupta31@users.noreply.github.com>
2024-07-24 10:24:46 +0530

    Update api-client.js

--- Client/src/lib/api-client.js
+++ Client/src/lib/api-client.js
@@ -1,5 +1,9 @@
 import axios from "axios"
 import {HOST} from "../utils/constants.js"
+
+axios.defaults.headers.get['Accepts'] = 'application/json';
+axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
+axios.defaults.headers.common['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept';
 export const apiClient=axios.create({
     baseURL:HOST,
 });
